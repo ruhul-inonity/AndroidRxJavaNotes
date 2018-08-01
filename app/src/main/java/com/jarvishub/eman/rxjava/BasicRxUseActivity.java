d9e65d4277893a84507f73677fb645ca7ece4bac
mrrobot <you@example.com>
2018-08-01 14:00:05 +0600

    init with basics

---
+++ app/src/main/java/com/jarvishub/eman/rxjava/BasicRxUseActivity.java
@@ -0,0 +1,207 @@
+package com.jarvishub.eman.rxjava;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class BasicRxUseActivity extends AppCompatActivity {
+
+    private static final String TAG = "BasicRxUseActivity";
+    private CompositeDisposable disposable = new CompositeDisposable();
+
+
+    @SuppressLint("CheckResult")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //////////example of custom Data Type and Operator
+
+        // add to Composite observable
+        // .map() operator is used to turn the note into all uppercase letters
+        disposable.add(getNotesObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<Note, Note>() {
+                    @Override
+                    public Note apply(Note note) throws Exception {
+                        // Making the note to all uppercase
+                        note.setNote(note.getNote().toUpperCase());
+                        return note;
+                    }
+                })
+                .subscribeWith(getNotesObserver()));
+
+
+ /*
+
+
+        //////// Example of multiple observable and CompositeDisposable
+
+
+   // observable
+        Observable<String> animalsObservable = getAnimalsObservable();
+
+        // observer
+        Observer<String> animalsObserver = getAnimalsObserver();
+
+        *//*
+        // observer subscribing to observable
+        animalsObservable
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);*//*
+
+        // observer subscribing to observable
+        animalsObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String s) throws Exception {
+                        return s.toLowerCase().startsWith("b");
+                    }
+                })
+                .subscribeWith(animalsObserver);*/
+
+    }
+
+    private Observer<String> getAnimalsObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+ /*   private Observable<String> getAnimalsObservable() {
+        return Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+    }*/
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        disposable.dispose();
+    }
+
+    private Observable<String> getAnimalsObservable() {
+        return Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog");
+    }
+
+    private List<Note> prepareNotes() {
+        List<Note> notes = new ArrayList<>();
+        notes.add(new Note(1, "buy tooth paste!"));
+        notes.add(new Note(2, "call brother!"));
+        notes.add(new Note(3, "watch narcos tonight!"));
+        notes.add(new Note(4, "pay power bill!"));
+
+        return notes;
+    }
+
+    class Note {
+        int id;
+        String note;
+
+        public Note(int id, String note) {
+            this.id = id;
+            this.note = note;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+    }
+
+    private DisposableObserver<Note> getNotesObserver() {
+        return new DisposableObserver<Note>() {
+
+            @Override
+            public void onNext(Note note) {
+                Log.d(TAG, "Note: " + note.getNote());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All notes are emitted!");
+            }
+        };
+    }
+
+    private Observable<Note> getNotesObservable() {
+        final List<Note> notes = prepareNotes();
+
+        return Observable.create(new ObservableOnSubscribe<Note>() {
+            @Override
+            public void subscribe(ObservableEmitter<Note> emitter) throws Exception {
+                for (Note note : notes) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(note);
+                    }
+                }
+
+                if (!emitter.isDisposed()) {
+                    emitter.onComplete();
+                }
+            }
+        });
+    }
+
+
+}
